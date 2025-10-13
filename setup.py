6769a45862dacc5832047215d57c5c2337d1f6a6
Fabian Jankowski <fjankowsk@gmail.com>
2025-10-13 21:33:29 +0200

    Move most of the packaging parameters from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os.path
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def get_version():
@@ -14,45 +14,6 @@ def get_version():
     return items["__version__"]
 
 
-def get_long_description():
-    with open("README.md", "r") as fd:
-        long_description = fd.read()
-
-    return long_description
-
-
 setup(
-    name="fitpdf",
     version=get_version(),
-    author="Fabian Jankowski",
-    author_email="fjankowsk@gmail.com",
-    description="Distribution fitting tools.",
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/fjankowsk/fitpdf",
-    license="MIT",
-    packages=find_packages(),
-    install_requires=[
-        "arviz",
-        "corner",
-        "KDEpy",
-        "matplotlib",
-        "numpy",
-        "pandas",
-        "pymc",
-        "scipy",
-        "xarray",
-    ],
-    entry_points={
-        "console_scripts": [
-            "fitpdf-fit = fitpdf.apps.fit_pdf:main",
-        ],
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    zip_safe=False,
 )
