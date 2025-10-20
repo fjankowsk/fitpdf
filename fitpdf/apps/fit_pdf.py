fdcc89005ee8cb28c3df02001f4786904b976261
Fabian Jankowski <fjankowsk@gmail.com>
2025-10-20 18:19:40 +0200

    Disable NNP for the moment. It requires more work.

--- fitpdf/apps/fit_pdf.py
+++ fitpdf/apps/fit_pdf.py
@@ -93,7 +93,7 @@ def parse_args():
     parser.add_argument(
         "--model",
         dest="model",
-        choices=["NL", "NN", "NNL", "NNP"],
+        choices=["NL", "NN", "NNL"],
         default="NNL",
         help="Use the specified distribution model, where N denotes a Normal and L a Lognormal component. For instance, the default NNL model consists of two Normal and one Lognormal distributions.",
     )
@@ -211,8 +211,6 @@ def fit_pe_dist(t_data, t_offp, params):
         mobj = fmodels.NN()
     elif params["model"] == "NNL":
         mobj = fmodels.NNL()
-    elif params["model"] == "NNP":
-        mobj = fmodels.NNP()
     else:
         raise NotImplementedError("Model not implemented: %s", params["model"])
 
