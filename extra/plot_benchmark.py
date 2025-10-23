8d9fb1683ad3f6044fc7e581945ef34cdd399a86
Fabian Jankowski <fjankowsk@gmail.com>
2025-10-23 16:19:11 +0200

    Add delta / sigma plot. Output plots to PDF files. Switch to using the full MCMC run for the benchmark.

--- extra/plot_benchmark.py
+++ extra/plot_benchmark.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from fitpdf.general_helpers import customise_matplotlib_format
+
+
+customise_matplotlib_format()
 
 files = glob.glob("fit_result_*.csv")
 files = sorted(files)
@@ -38,7 +42,11 @@ df_results.index = np.arange(len(df_results.index))
 print(df_results)
 df_results.info()
 
+params = {"dpi": 300}
+
 # parameter recovery accuracy
+true_val = 0.2
+
 fig = plt.figure()
 ax = fig.add_subplot()
 
@@ -46,24 +54,61 @@ ax.errorbar(
     df_results["nsamp"], df_results["mean"], yerr=df_results["std"], fmt="x", zorder=4
 )
 
-ax.axhline(y=0.2, ls="dashed", lw=2, color="C1", zorder=3)
+ax.axhline(y=true_val, ls="dashed", lw=2, color="C1", zorder=3)
 
 ax.set_xlabel("Number of samples")
 ax.set_ylabel("Fit value")
 
 fig.tight_layout()
+
+fig.savefig(
+    "benchmark_accuracy.pdf",
+    bbox_inches="tight",
+    dpi=params["dpi"],
+)
+
+# parameter delta
+fig = plt.figure()
+ax = fig.add_subplot()
+
+data_lo = (df_results["mean"] - df_results["std"] - true_val) / df_results["std"]
+data_hi = (df_results["mean"] + df_results["std"] - true_val) / df_results["std"]
+data_mean = 0.5 * (data_lo + data_hi)
+
+ax.fill_between(df_results["nsamp"], data_lo, data_hi, color="lightgray", zorder=3)
+
+ax.scatter(df_results["nsamp"], data_mean, marker="x", zorder=5)
+
+ax.axhline(y=0, ls="dashed", lw=2, color="C0", zorder=4)
+
+ax.set_xlabel("Number of samples")
+ax.set_ylabel("Delta (std)")
+
+fig.tight_layout()
+
+fig.savefig(
+    "benchmark_delta_std.pdf",
+    bbox_inches="tight",
+    dpi=params["dpi"],
+)
 
 # precision and spread
 fig = plt.figure()
 ax = fig.add_subplot()
 
-ax.hist(df_results["mean"], bins="auto", histtype="step", lw=2)
+ax.hist(df_results["mean"], bins="auto", histtype="step", lw=2, zorder=3)
 
-ax.axhline(y=0.2, ls="dashed", lw=2, color="C1", zorder=3)
+ax.axvline(x=true_val, ls="dashed", lw=2, color="C1", zorder=5)
 
 ax.set_xlabel("Number of samples")
 ax.set_ylabel("Fit value")
 
 fig.tight_layout()
+
+fig.savefig(
+    "benchmark_histogram.pdf",
+    bbox_inches="tight",
+    dpi=params["dpi"],
+)
 
 plt.show()
